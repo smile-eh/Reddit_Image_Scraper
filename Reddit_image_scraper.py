eaf0b429653ce25fcb851b23a3e0aac355152cd2
crawsome <c3g6b2@gmail.com>
2020-05-13 12:14:13 -0700

    more updates!
    1. Download multiple subreddits! Edit subs.txt with the subreddits whose images you want to sync down, then run the script. Much easier than typing one-at-a-time, every time. 
    2. (TODO) Still trying to find a way to smartly handle / avoid some weird connection closed errors when encountering certain filenames. 
    3. Removed some excess print statements.
    4. Results now are categorized result/<subreddit name>. 
    5. Skips re-downloading files that you have already downloaded, so now acts more like a 1-way sync script. Awesome for running on a CHRON job or scheduled task! Make sure you have free HDD space before you run!
    6. Removed gifv because it was downloading HTML instead. 
    7. Increased verbosity of error messages for better logging
    8. Searched formats currently include '.webm', '.gif', '.avi', '.mp4', '.jpg', '.png', '.mov', '.ogg', '.wmv', 'mp2', 'mp3', 'mkv'
    9. (TODO) gyfcat is troublesome because their results are proprietary and PRAW doesn't interact with it well. 
    
    More updates / overhauls to come as improvements present themself!

--- Reddit_image_scraper.py
+++ Reddit_image_scraper.py
@@ -2,11 +2,18 @@ import praw
 import configparser
 import urllib.request
 import re
+import os
 from time import sleep
 
 from prawcore.exceptions import Redirect
 from prawcore.exceptions import ResponseException
 from urllib.error import HTTPError
+
+# Reddit Image Sync
+# 2020 Colin Burke
+# Scrapes all subreddits defined in subs.txt for (default) 20k max queries each.
+# reminder, this is not meant to be a fast operation. Each download takes 2 sec to avoid rate limiting / premature D/C.
+# Set it and forget it overnight.
 
 
 class ClientInfo:
@@ -20,8 +27,9 @@ def get_client_info():
     config.read("config.ini")
     id = config["ALPHA"]["client_id"]
     secret = config["ALPHA"]["client_secret"]
+    query_limit = config["ALPHA"]["query_limit"]
 
-    return id, secret
+    return id, secret, query_limit
 
 
 def save_list(img_url_list):
@@ -37,16 +45,17 @@ def delete_img_list():
 
 
 def is_img_link(img_link):
+    # print('Original Link:' + img_link) # enable this if you want to log the literal URLs it sees
     regex = '([.][\w]+)$'
     re.compile(regex)
-    print(img_link)
     t = re.search(regex, img_link)
+
+    # extension is the last 3 characters, unless it matches the regex.
     ext = img_link[-4:]
     if t:
         ext = t.group()
-        print(ext)
 
-    if ext in ('.webm', '.gif', '.avi', '.mp4', '.gifv', '.jpg', '.png', '.mov'):
+    if ext in ('.webm', '.gif', '.avi', '.mp4', '.jpg', '.png', '.mov', '.ogg', '.wmv', 'mp2', 'mp3', 'mkv'):
         return True
     else:
         return False
@@ -59,87 +68,100 @@ def get_img_urls(sub, li):
         return [submission.url for submission in submissions]
 
     except Redirect:
-        print("Invalid Subreddit!")
+        print("get_img_urls() Redirect. Invalid Subreddit?")
         return 0
 
     except HTTPError:
-        print("Too many Requests. Try again later!")
+        print("get_img_urls() HTTPError in last query")
         sleep(10)
         return 0
 
     except ResponseException:
-        print("Client info is wrong. Check again.")
+        print("get_img_urls() ResponseException.")
         return 0
 
 
-def download_img(img_url, img_title, filename):
-    print(img_url + ' ' + img_title + ' ' + filename)
+def download_img(img_url, img_title, file_loc, sub):
+    # print(img_url + ' ' + img_title + ' ' + file_loc)
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     try:
-        print('Downloading ' + img_title + '....')
-        urllib.request.urlretrieve(img_url, filename)
+        print('Sub:{} Filename:{}'.format(sub, img_title))
+        urllib.request.urlretrieve(img_url, file_loc)
         sleep(2)  # this is necessary so you can download the whole sub
         return 1
 
     except HTTPError:
-        print("Too many Requests. Try again later!")
+        print("download_img() HTTPError in last query")
         sleep(10)
-        return -1
+        return 1
 
     except urllib.error.URLError:
-        print("URLError!")
+        print("download_img() URLError!")
         sleep(2)
         return 1
 
 
-def read_img_links():
+def read_img_links(sub):
     with open('img_links.txt') as f:
         links = f.readlines()
 
     links = [x.strip() for x in links]
     download_count = 0
+    download_status = 0
 
     for link in links:
         if not is_img_link(link):
             continue
 
         file_name = link.split('/')[-1]
-        file_loc = 'result/{}'.format(file_name)
+        if not os.path.exists('result/{}'.format(sub)):
+            os.mkdir('result/{}'.format(sub))
+        file_loc = 'result/{}/{}'.format(sub, file_name)
+        if os.path.exists(file_loc):
+            print(file_name + ' already exists')
+            continue
 
         if not file_name:
             print(file_name + ' cannot download')
             continue
 
-        download_status = download_img(link, file_name, file_loc)
+        download_status = download_img(link, file_name, file_loc, sub)
+
         download_count += 1
 
-        if download_status == 0:
-            return download_count, 0
-
-    return download_count, 1
+    return download_count, download_status
 
 
 if __name__ == '__main__':
-    delete_img_list()
+    going = True
+    f = open('./subs.txt', 'r')
 
-    ClientInfo.id, ClientInfo.secret = get_client_info()
+    for subreddit in f.readlines():
+        if '#' in subreddit:
+            continue
+        subreddit = subreddit.strip('\n')
+        print('Starting Retrieval from: /r/' + subreddit)
+        delete_img_list()
 
-    subreddit = input('Enter Subreddit: ')
-    query_lookup_limit = int(input('Enter the max amount of queries: '))
-    print()
-    url_list = get_img_urls(subreddit, query_lookup_limit)
-    file_no = 1
+        ClientInfo.id, ClientInfo.secret, query_lookup_limit = get_client_info()
 
-    if url_list:
+        # subreddit = input('Enter Subreddit: ')
+        # query_lookup_limit = int(input('Enter the max amount of queries: '))
+        url_list = get_img_urls(subreddit, query_lookup_limit)
+        file_no = 1
 
-        save_list(url_list)
-        count, status = read_img_links()
+        if url_list:
+            try:
+                save_list(url_list)
+                count, status = read_img_links(subreddit)
 
-        if status == 1:
-            print('\nDownload Complete\n{} - Images Downloaded\n{} - Posts Not found, or ignored'.format(count,
-                                                                                                         query_lookup_limit - count))
-        elif status == 0:
-            print('\nDownload Incomplete\n{} - Images Downloaded'.format(count))
-    delete_img_list()
+                if status == 1:
+                    print('\nDownload Complete\n{} - Images Downloaded\nQuery limit: {} '.format(count,
+                                                                                                 query_lookup_limit))
+                elif status == 0:
+                    print('\nDownload Incomplete\n{} - Images Downloaded'.format(count))
+            except UnicodeEncodeError:
+                print('unicode error')
+        delete_img_list()
