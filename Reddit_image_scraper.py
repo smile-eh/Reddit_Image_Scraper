f5670236fe1025284ec756413810f09442bbc1ae
crawsome <c3g6b2@gmail.com>
2020-05-16 12:51:30 -0700

    added gfycat support

--- Reddit_image_scraper.py
+++ Reddit_image_scraper.py
@@ -9,16 +9,24 @@ from prawcore.exceptions import Redirect
 from prawcore.exceptions import ResponseException
 from urllib.error import HTTPError
 
+
 # Reddit Image Sync
 # 2020 Colin Burke
 # Scrapes all subreddits defined in subs.txt for (default) 20k max queries each.
 # reminder, this is not meant to be a fast operation. Each download takes 2 sec to avoid rate limiting / premature D/C.
-# Set it and forget it overnight, run it when you want to get the newest images, downloaded locally to your PC. 
+# Set it and forget it overnight.
+#todo logging in each folder
+#todo restrict file size so no error messages are downloaded
 
 class ClientInfo:
     id = ''
     secret = ''
     user_agent = 'Reddit_Image_Scraper'
+
+
+def delete_img_list():
+    f = open('img_links.txt', 'r+')
+    f.truncate()
 
 
 def get_client_info():
@@ -29,8 +37,9 @@ def get_client_info():
     query_limit = config["ALPHA"]["query_limit"]
     ratelimit_sleep = config["ALPHA"]["ratelimit_sleep"]
     failure_sleep = config["ALPHA"]["failure_sleep"]
+    minimum_file_size_kb = config["ALPHA"]["minimum_file_size_kb"] # not implemented yet
 
-    return id, secret, int(query_limit), int(ratelimit_sleep), int(failure_sleep)
+    return id, secret, int(query_limit), int(ratelimit_sleep), int(failure_sleep), int(minimum_file_size_kb * 1024)
 
 
 def save_list(img_url_list):
@@ -40,9 +49,14 @@ def save_list(img_url_list):
         file.close()
 
 
-def delete_img_list():
-    f = open('img_links.txt', 'r+')
-    f.truncate()
+def clean_sub_files(sub):
+    """
+
+    :param sub:str
+    :return:
+    """
+    if os.path.exists('result/{}'.format(sub)):
+        pass
 
 
 def is_media_file(uri):
@@ -55,7 +69,6 @@ def is_media_file(uri):
     ext = uri[-4:]
     if t:
         ext = t.group()
-
     if ext in ('.webm', '.gif', '.avi', '.mp4', '.jpg', '.png', '.mov', '.ogg', '.wmv', 'mp2', 'mp3', 'mkv'):
         return True
     else:
@@ -66,9 +79,7 @@ def get_img_urls(sub, limit):
     try:
         r = praw.Reddit(client_id=ClientInfo.id, client_secret=ClientInfo.secret, user_agent=ClientInfo.user_agent)
         submissions = r.subreddit(sub).top(time_filter='all', limit=limit)
-        r = [submission.url for submission in submissions]
-        print(r)
-        return r
+        return [submission.url for submission in submissions]
     except Redirect:
         print("get_img_urls() Redirect. Invalid Subreddit?")
         return 0
@@ -83,19 +94,24 @@ def get_img_urls(sub, limit):
         return 0
 
 
-def download_img(img_url, img_title, file_loc, sub, ratelimit_sleep:int, failure_sleep:int):
+def download_img(img_url, img_title, file_loc, sub, ratelimit_sleep: int, failure_sleep: int):
     # print(img_url + ' ' + img_title + ' ' + file_loc)
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     try:
-        print('Subreddit:/r/{} Filename:{}'.format(sub, img_title))
+        print('Subreddit: /r/{} Filename: {}\nFull URL: {}'.format(sub, img_title, img_url))
+        #u = urllib.request.urlopen(img_url)
+        #u_metadata = u.info()
+        #size = int(u_metadata.getheaders("Content-Length")[0])
+        #print(size)
+
         urllib.request.urlretrieve(img_url, file_loc)
         sleep(ratelimit_sleep)  # this is necessary so you can download the whole sub
         return 1
 
     except HTTPError:
-        print("download_img() HTTPError in last query")
+        print("download_img() HTTPError in last query (file might not exist anymore, or malformed URL)")
         sleep(failure_sleep)
         return 1
 
@@ -114,6 +130,11 @@ def read_img_links(sub):
     download_status = 0
 
     for link in links:
+
+        if 'gfycat.com' in link and '.gif' not in link[-4:]:
+            # print(link[-4:])
+            # print('gfycat found:{}'.format(link))
+            link = link + '.gif'
         if not is_media_file(link):
             continue
 
@@ -145,9 +166,8 @@ if __name__ == '__main__':
             continue
         subreddit = subreddit.strip('\n')
         print('Starting Retrieval from: /r/' + subreddit)
-        delete_img_list()
 
-        ClientInfo.id, ClientInfo.secret, query_lookup_limit, ratelimit_sleep, failure_sleep = get_client_info()
+        ClientInfo.id, ClientInfo.secret, query_lookup_limit, ratelimit_sleep, failure_sleep, minimum_file_size_kb = get_client_info()
 
         # subreddit = input('Enter Subreddit: ')
         # query_lookup_limit = int(input('Enter the max amount of queries: '))
@@ -160,10 +180,12 @@ if __name__ == '__main__':
                 count, status = read_img_links(subreddit)
 
                 if status == 1:
-                    print('\nDownload Complete\n{} - Images Downloaded\nQuery limit: {} '.format(count,
-                                                                                                 query_lookup_limit))
+                    print('Download Complete from {}\n{} - Images Downloaded\nQuery limit: {} \n'.format(
+                        subreddit, count, query_lookup_limit))
                 elif status == 0:
-                    print('\nDownload Incomplete\n{} - Images Downloaded'.format(count))
+                    print('Download Incomplete\n{} - Images Downloaded\n'.format(count))
             except UnicodeEncodeError:
-                print('unicode error')
+                print('UnicodeEncodeError:{}\n'.format(subreddit))
+            except OSError:
+                print('OSError:{}\n'.format(subreddit))
         delete_img_list()
