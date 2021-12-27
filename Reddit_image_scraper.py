ad81650b878a9195fbfa98ce04debd166540faf2
C. Burke <c3g6b2@gmail.com>
2021-12-27 13:18:23 -0500

    many improvements
    removed need for external subs.txt, 
    dedup for badlist
    badlist auto-generation for filesizes under X KB (Defined in config.ini)
    gets all possible sub queries 'hour', 'month', 'all', 'week', 'year', 'day' / hot/new/top
    it will time each query to see when the API is slowing you down. Tune the config.ini's error timeout / query timeouts to adjust.
    created a logfile generation instead of just printing to the screen. It will still print anyway. Check your download logs for any events / how your last scrape went.

--- Reddit_image_scraper.py
+++ Reddit_image_scraper.py
@@ -1,22 +1,34 @@
+# Reddit Image Sync
+# 2021 Colin Burke
+# Scrapes all subreddits defined in subs.txt for (default) 20k max queries each.
+# reminder, this is not meant to be a fast operation. Each download takes 2 sec to avoid rate limiting / premature D/C.
+# Set it and forget it overnight.
+
 import praw
 import configparser
 import urllib.request
 import re
 import os
-from time import sleep
-import time
 
 from prawcore.exceptions import Redirect
 from prawcore.exceptions import ResponseException
 from urllib.error import HTTPError
 import http.client
+import time
+from time import sleep
+from datetime import datetime
+
+now = datetime.now()
+dt_name = now.strftime("%d-%m-%Y-%H.%M.%S")
+logfile = open('./logs/{}.txt'.format(dt_name), 'w')
 
 
-# Reddit Image Sync
-# 2021 Colin Burke
-# Scrapes all subreddits defined in subs.txt for (default) 2k max queries each.
-# reminder, this is not meant to be a fast operation. Each download takes 2 sec to avoid rate limiting / premature D/C.
-# Set it and forget it overnight.
+def log(text):
+    print(text)
+    now = datetime.now()
+    event_time = now.strftime("%d-%m-%Y-%H.%M.%S")
+    logfile.write('{}: {}\n'.format(event_time, text))
+
 
 class ClientInfo:
     id = ''
@@ -24,10 +36,33 @@ class ClientInfo:
     user_agent = 'Reddit_Image_Scraper'
 
 
-def add_to_badlist(title):
+def badlist_cleanup(minimum_file_size_kb):
+    """removes small files, cleans and de-dupes the badlist"""
+
+    # crawl all directories recursively, find files lower than 15kb, add them to the badlist, then delete them.
+    for root, subdirs, files in os.walk('./result'):
+        for file in files:
+            fullpath = os.path.join(root, file)
+            filesize = os.path.getsize(fullpath) / 1000
+            if 'gitignore' in fullpath:
+                continue
+            if filesize < minimum_file_size_kb:
+                add_to_badlist(file)
+                print(str(fullpath) + " \t" + str(filesize) + "KB")
+                os.remove(fullpath)
+
+    # de-duplicate the badlist.
+    with open('badlist.txt', 'r') as f:
+        in_list = set(f.readlines())
+
+    # we-write the badlist back to itself.
+    with open('badlist.txt', 'w') as f:
+        f.writelines(in_list)
+
+def add_to_badlist(filename):
     with open('badlist.txt', 'a') as f:
-        f.writelines(title + '\n')
-        print("added to badlist")
+        f.writelines(filename + '\n')
+        logfile.write("added {} to badlist".format(filename))
 
 
 def get_client_info():
@@ -39,21 +74,7 @@ def get_client_info():
     ratelimit_sleep = config["ALPHA"]["ratelimit_sleep"]
     failure_sleep = config["ALPHA"]["failure_sleep"]
     minimum_file_size_kb = config["ALPHA"]["minimum_file_size_kb"]
-
-    return id, secret, int(query_limit), int(ratelimit_sleep), int(failure_sleep), int(minimum_file_size_kb * 1024)
-
-
-def save_list(img_url_list):
-    for img_url in img_url_list:
-        file = open('img_links.txt', 'a')
-        file.write('{} \n'.format(img_url))
-        file.close()
-
-
-def delete_img_list():
-    f = open('img_links.txt', 'w')
-    f.truncate()
-
+    return id, secret, int(query_limit), int(ratelimit_sleep), int(failure_sleep), float(minimum_file_size_kb)
 
 def is_media_file(uri):
     # print('Original Link:' + img_link) # enable this if you want to log the literal URLs it sees
@@ -61,52 +82,88 @@ def is_media_file(uri):
     re.compile(regex)
     t = re.search(regex, uri)
 
-    # extension is the last 3 characters, unless it matches the regex.
+    # extension is in the last 4 characters, unless it matches the regex.
     ext = uri[-4:]
     if t:
         ext = t.group()
-    if ext in ('.webm', '.gif', '.avi', '.mp4', '.jpg', '.png', '.mov', '.ogg', '.wmv', 'mp2', 'mp3', 'mkv'):
+    if ext in ('.webm', '.gif', '.avi', '.mp4', '.jpg', '.png', '.mov', '.ogg', '.wmv', '.mp2', '.mp3', '.mkv'):
         return True
     else:
         return False
 
 
 def get_img_urls(sub, limit):
+    # TODO how do I automate these generators? 
+    time_filters = ['hour', 'month', 'all', 'week', 'year', 'day']
     try:
+        all_start = time.time()
         r = praw.Reddit(client_id=ClientInfo.id, client_secret=ClientInfo.secret, user_agent=ClientInfo.user_agent)
         start = time.time()
         submissions = [submission.url for submission in r.subreddit(sub).top(time_filter='all', limit=limit)]
         end = time.time()
-        print('Query return time for ALL:' + str(end-start))
+        log('Query return time for ALL:{},\nTotal Found: {}'.format(str(end - start), len(submissions)))
 
         start = time.time()
-        submissions2 = [submission.url for submission in r.subreddit(sub).hot(limit=limit)]
+        submissions2 = [submission.url for submission in r.subreddit(sub).top(time_filter='year', limit=limit)]
         end = time.time()
-        print('Query return time for HOT:' + str(end-start))
+        log('Query return time for year:{},\nTotal Found: {}'.format(str(end - start), len(submissions2)))
 
         start = time.time()
-        submissions3 = [submission.url for submission in r.subreddit(sub).new(limit=limit)]
+        submissions3 = [submission.url for submission in r.subreddit(sub).top(time_filter='month', limit=limit)]
         end = time.time()
-        print('Query return time for NEW:' + str(end-start))
+        log('Query return time for month:{},\nTotal Found: {}'.format(str(end - start), len(submissions3)))
 
         start = time.time()
-        submissions4 = [submission.url for submission in r.subreddit(sub).rising(limit=limit)]
+        submissions4 = [submission.url for submission in r.subreddit(sub).top(time_filter='week', limit=limit)]
         end = time.time()
-        print('Query return time for RISING:' + str(end-start))
+        log('Query return time for week:{},\nTotal Found: {}'.format(str(end - start), len(submissions4)))
 
-        submissions = list(set(submissions + submissions2 + submissions3 + submissions4))
+        start = time.time()
+        submissions5 = [submission.url for submission in r.subreddit(sub).top(time_filter='hour', limit=limit)]
+        end = time.time()
+        log('Query return time for hour:{},\nTotal Found: {}'.format(str(end - start), len(submissions5)))
+
+        start = time.time()
+        submissions6 = [submission.url for submission in r.subreddit(sub).top(time_filter='day', limit=limit)]
+        end = time.time()
+        log('Query return time for day:{},\nTotal Found: {}'.format(str(end - start), len(submissions6)))
+
+        start = time.time()
+        submissions7 = [submission.url for submission in r.subreddit(sub).hot(limit=limit)]
+        end = time.time()
+        log('Query return time for HOT:{},\nTotal Found: {}'.format(str(end - start), len(submissions7)))
+
+        start = time.time()
+        submissions8 = [submission.url for submission in r.subreddit(sub).new(limit=limit)]
+        end = time.time()
+        log('Query return time for NEW:{},\nTotal Found: {}'.format(str(end - start), len(submissions8)))
+
+        start = time.time()
+        submissions9 = [submission.url for submission in r.subreddit(sub).rising(limit=limit)]
+        end = time.time()
+        log('Query return time for RISING:{},\nTotal Found: {}'.format(str(end - start), len(submissions9)))
+
+        # combine them all, and de-duplicate them
+        submissions = list(set(
+            submissions + submissions2 + submissions3 + submissions4 + submissions5 + submissions6 + submissions7 + submissions8 + submissions9))
+
+        log('total unique submissions: {}'.format(len(submissions)))
+
+        all_end = time.time()
+        log('Query return time for :{}: {}'.format(str(sub), str(all_end - all_start)))
         return submissions
+
     except Redirect:
-        print("get_img_urls() Redirect. Invalid Subreddit?")
+        log("get_img_urls() Redirect. Invalid Subreddit?")
         return 0
 
     except HTTPError:
-        print("get_img_urls() HTTPError in last query")
+        log("get_img_urls() HTTPError in last query")
         sleep(10)
         return 0
 
     except ResponseException:
-        print("get_img_urls() ResponseException.")
+        log("get_img_urls() ResponseException.")
         return 0
 
 
@@ -116,7 +173,7 @@ def download_img(img_url, img_title, file_loc, sub, ratelimit_sleep: int, failur
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     try:
-        print('Subreddit: /r/{} Filename: {}\nFull URL: {}'.format(sub, img_title, img_url))
+        log('Subreddit: /r/{} - Full URL: {}'.format(sub, img_title, img_url))
         u = urllib.request.urlopen(img_url)
         u_metadata = u.info()
         # size = int(u_metadata.getheaders("Content-Length")[0])
@@ -126,45 +183,44 @@ def download_img(img_url, img_title, file_loc, sub, ratelimit_sleep: int, failur
         sleep(ratelimit_sleep)  # remove this at your own risk. This is necessary so you can download the whole sub!
         return 0
 
-    except HTTPError:
-        print("download_img() HTTPError in last query (file might not exist anymore, or malformed URL)")
+    except HTTPError as e:
+        log("download_img() HTTPError in last query (file might not exist anymore, or malformed URL)")
         with open('badlist.txt', 'a') as f:
-            f.writelines(img_title+'\n')
-            print("added to badlist")
+            f.writelines(img_title + '\n')
+            log("added to badlist")
+        log(e)
         sleep(failure_sleep)
         return 1
 
-    except urllib.error.URLError:
-        print("download_img() URLError! Site might be offline")
+    except urllib.error.URLError as e:
+        log("download_img() URLError! Site might be offline")
+        log(e)
         with open('badlist.txt', 'a') as f:
-            f.writelines(img_title+'\n')
-            print("added to badlist")
+            f.writelines(img_title + '\n')
+            log("added to badlist")
         sleep(ratelimit_sleep)
         return 1
 
-    except http.client.InvalidURL:
-        print('Crappy URL')
-        with open('badlist.txt', 'a') as f:
-            f.writelines(img_title+'\n')
-            print("added to badlist")
+    except http.client.InvalidURL as e:
+        log('Crappy URL')
+        log(e)
+        add_to_badlist(img_title)
         return 1
 
 
-def read_img_links(sub):
-    with open('img_links.txt') as f:
-        links = f.readlines()
+def read_img_links(sub,url_list):
 
-    links = [x.strip() for x in links]
-    links.sort()
+    url_list = [x.strip() for x in url_list]
+    url_list.sort()
     download_count = 0
     exist_count = 0
     download_status = 0
-    badlist = []
+
     with open('badlist.txt', 'r') as f:
         badlist = f.readlines()
         badlist = [x.strip() for x in set(badlist)]
 
-    for link in links:
+    for link in url_list:
         if 'gfycat.com' in link and '.gif' not in link[-4:] and '.webm' not in link[-4:]:
             # print(link[-4:])
             # print('gfycat found:{}'.format(link))
@@ -174,18 +230,18 @@ def read_img_links(sub):
 
         file_name = link.split('/')[-1]
         if file_name in badlist:
-            #print('{} found in badlist, skipping'.format(file_name))
+            # print('{} found in badlist, skipping'.format(file_name))
             continue
         if not os.path.exists('result/{}'.format(sub)):
             os.mkdir('result/{}'.format(sub))
         file_loc = 'result/{}/{}'.format(sub, file_name)
         if os.path.exists(file_loc):
-            #print(file_name + ' already exists')
+            # print(file_name + ' already exists')
             exist_count += 1
             continue
 
         if not file_name:
-            print(file_name + ' cannot download')
+            log(file_name + ' cannot download')
             continue
 
         download_status = download_img(link, file_name, file_loc, sub, ratelimit_sleep, failure_sleep)
@@ -195,7 +251,10 @@ def read_img_links(sub):
 
 
 if __name__ == '__main__':
-    delete_img_list()
+    ClientInfo.id, ClientInfo.secret, query_lookup_limit, ratelimit_sleep, failure_sleep, minimum_file_size_kb = get_client_info()
+    # if exited unexpectedly, clean up the image list file.
+    badlist_cleanup(minimum_file_size_kb)
+
     going = True
     f = open('./subs.txt', 'r')
 
@@ -203,33 +262,30 @@ if __name__ == '__main__':
         if '#' in subreddit:
             continue
         subreddit = subreddit.strip('\n')
-        print('Starting Retrieval from: /r/' + subreddit)
-
-        ClientInfo.id, ClientInfo.secret, query_lookup_limit, ratelimit_sleep, failure_sleep, minimum_file_size_kb = get_client_info()
+        log('Starting Retrieval from: /r/' + subreddit)
 
         # subreddit = input('Enter Subreddit: ')
         # query_lookup_limit = int(input('Enter the max amount of queries: '))
+        url_list = []
         url_list = get_img_urls(subreddit, query_lookup_limit)
         file_no = 1
 
         if url_list:
             try:
-                print('{} images found on {}'.format(len(url_list),subreddit))
-                save_list(url_list)
-                count, status, already_here = read_img_links(subreddit)
+                log('{} images found on {}'.format(len(url_list), subreddit))
+                count, status, already_here = read_img_links(subreddit, url_list)
 
                 if status == 1:
-                    print(
+                    log(
                         'Download Complete from {}\n{} - Images Downloaded\nQuery limit: {} \nAlready downloaded: {} \n'.format(
-                            subreddit, count, query_lookup_limit,already_here))
+                            subreddit, count, query_lookup_limit, already_here))
                 elif status == 0:
-                    print(
+                    log(
                         'Download Complete from {}\n{} - Images Downloaded\nQuery limit: {} \nAlready downloaded: {} \n'.format(
-                            subreddit, count, query_lookup_limit,already_here))
+                            subreddit, count, query_lookup_limit, already_here))
             except UnicodeEncodeError:
-                print('UnicodeEncodeError:{}\n'.format(subreddit))
+                log('UnicodeEncodeError:{}\n'.format(subreddit))
             except OSError as e:
 
-                print('OSError:{}\nVerbose:{}'.format(subreddit, e))
-        delete_img_list()
-        #confirm = input('confirm next sub? CTRL+C to cancel.')
+                log('OSError:{}\nVerbose:{}'.format(subreddit, e))
+        # confirm = input('confirm next sub? CTRL+C to cancel.')
